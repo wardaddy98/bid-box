8df41d906a0a87514dec4a800f3e55de5c7c91dc
wardaddy98 <suddath.gautam98@gmail.com>
2026-01-13 08:34:20 +0530

    morning dump

--- src/components/NavButton/index.tsx
+++ src/components/NavButton/index.tsx
@@ -11,7 +11,7 @@ const NavButton = (props: Props) => {
   const { href, label, variant, className = '' } = props;
   return (
     <Link
-      className={`${variant === 'primary' ? 'bg-primary text-white hover:bg-gray-500 shadow-md rounded-sm px-5 py-2' : variant === 'secondary' ? 'bg-gray-100 text-black hover:bg-gray-500 hover:text-white shadow-md rounded-sm px-5 py-2' : variant === 'text' ? 'text-gray-500 hover:text-primary hover:underline' : ''} transition-all  text-sm font-medium ${className}`}
+      className={`${variant === 'primary' ? 'bg-primary text-white hover:bg-gray-500 shadow-md rounded-sm px-5 py-2' : variant === 'secondary' ? 'bg-gray-200 text-black hover:bg-gray-500 hover:text-white shadow-md rounded-sm px-5 py-2' : variant === 'text' ? 'text-black hover:text-primary hover:underline' : ''} transition-all  text-sm font-medium ${className}`}
       href={href}
     >
       {label}
