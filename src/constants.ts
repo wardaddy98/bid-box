53afabffd42c47c71d818cc5d1f6fe5f9dff9eca
wardaddy98 <suddath.gautam98@gmail.com>
2026-06-19 20:01:29 +0530

    separating socket and api url

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,14 @@
 interface IConstants {
-  API_URL: string | undefined;
+  NEXT_PUBLIC_API_URL: string | undefined;
+  NEXT_PUBLIC_SOCKET_URL: string | undefined;
   NEXT_PUBLIC_RAZORPAY_KEY_ID: string | undefined;
   NEXT_PUBLIC_COMPANY_LOGO: string | undefined;
   NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID: string;
 }
 
 const constants: IConstants = {
-  API_URL: process.env.NEXT_PUBLIC_API_URL,
+  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL,
   NEXT_PUBLIC_RAZORPAY_KEY_ID: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
   NEXT_PUBLIC_COMPANY_LOGO: process.env.NEXT_PUBLIC_COMPANY_LOGO,
   NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID ?? '',
